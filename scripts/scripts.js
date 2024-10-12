f841444ee17e392360e3495526f81fa6d8998366
nicolascorderodev <nicolascordero281@gmail.com>
2024-10-11 23:16:14 -0500

    Empezamos con JS

---
+++ scripts/scripts.js
@@ -0,0 +1,37 @@
+//TEMA 1
+alert("Hola mundo");
+//TEMA 1
+
+
+//TEMA 2
+//literales
+12.57                   //número decimal
+12                      //número entero
+-12                     //número negativo
+"¡Hola mundo!"          //cadena de caracteres con comillas dobles
+'¡Hola mundo!'          //cadena de caracteres con comillas simples
+
+//variables
+var x;                  //declarar una variable sin valor inicial
+var x = 5;              //declarar variable con valor inicial
+var x = "¡Hola mundo!"  //declarar una variable con un string asignado
+
+
+//Como escribir correctamente una variable
+
+// var var = 5; Incorrecta, utiliza var que es una palabra reservada.
+
+//var vari*able = 5; Incorrecta, utiliza un caracter especial para el nombre de la variable
+
+//var mi_variable = 5; Correcta, los simbolos "-" y "_" son válidos.
+
+
+
+//Copiar el valor de la primera variable a la segunda
+
+var a = 5;
+var b = a;//Después de correr esta línea de código, JavaScript tendrá declaradas las variables a y b con el valor numérico 5
+//TEMA 2
+
+
+//TEMA 3
